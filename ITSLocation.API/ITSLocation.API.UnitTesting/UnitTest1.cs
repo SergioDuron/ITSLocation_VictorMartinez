65c10937f629ac7b871bac98097fbcabd3acb3f4
VictorDmartz <victor.martinez@it-seekers.com>
2024-06-28 09:57:42 -0600

    Sprint 1
    Cascaron inicial de la aplicación

---
+++ ITSLocation.API/ITSLocation.API.UnitTesting/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ITSLocation.API.UnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
